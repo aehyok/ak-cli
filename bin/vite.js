0e5770c7509bd2f31f59704c0cedb029b597d070
刘启明 <455043818@qq.com>
2022-08-02 20:42:51 +0800

    fix: release-it-changelog50

---
+++ bin/vite.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+
+const result = "1234567890"
+console.log('vitess', result)
+
+console.log('vite.js')
---
+++ bin/vue.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+
+console.log('vue')
+console.log('vue.js')
